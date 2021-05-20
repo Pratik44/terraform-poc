6ccd4891dd9c826d7ccd07f222f00fa24069af93
Pratik44 <pratik.deenbandhu456@gmail.com>
2021-05-21 00:01:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,9 +12,9 @@ variable "AWS_REGION" {
     default = "ap-south-1"
 }
 
-variable "public_sub_id" {
-    value = ${module.subnet.public_sub_id}
-}
+#variable "public_sub_id" {
+#    value = ${module.subnet.public_sub_id}
+#}
 
 #variable "private_sub_id" {
 #type = "string"
