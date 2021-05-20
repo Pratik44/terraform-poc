5b2b9b45c60b06e5d353a977e5ffe25ae413a4da
Pratik44 <pratik.deenbandhu456@gmail.com>
2021-05-21 00:41:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -102,27 +102,6 @@ resource "aws_security_group" "web-allowed" {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-#variable "AMI" {
-#    type = "map"
-#    
-#    default = {
-#        ap-south-1 = "ami-011a9944eb4abcf55"
-#        ap-south-2 = "ami-011a9944eb4abcf55"
-#    }
-#}
-
-
 resource "aws_instance" "webserver" {
     ami = "${lookup(var.AMI, var.AWS_REGION)}"
     instance_type = "t2.micro"
@@ -146,3 +125,75 @@ resource "aws_ebs_volume" "secondary_disk" {
   size              = 1
 }
 
+
+resource "aws_elb" "web_elb" {
+  name = "web-elb"
+  security_groups = [
+    aws_security_group.web-allowed.id
+  ]
+  subnets = [ "${module.subnet.public_sub_id}" ]
+
+  cross_zone_load_balancing   = true
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:80/"
+  }
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "80"
+    instance_protocol = "http"
+  }
+
+}
+
+
+resource "aws_autoscaling_group" "web" {
+  name = "${aws_instance.webserver.tags.Name}-asg"
+
+  min_size             = 1
+  desired_capacity     = 2
+  max_size             = 4
+  
+  health_check_type    = "ELB"
+  load_balancers = [
+    aws_elb.web_elb.id
+  ]
+
+  launch_configuration = aws_instance.webserver.tags.Name
+
+  enabled_metrics = [
+    "GroupMinSize",
+    "GroupMaxSize",
+    "GroupDesiredCapacity",
+    "GroupInServiceInstances",
+    "GroupTotalInstances"
+  ]
+
+  metrics_granularity = "1Minute"
+
+  vpc_zone_identifier  = [
+    "${module.subnet.public_sub_id}"
+  ]
+
+  # Required to redeploy without an outage.
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "web"
+    propagate_at_launch = true
+  }
+
+}
+
+output "elb_dns_name" {
+  value = aws_elb.web_elb.dns_name
+}
